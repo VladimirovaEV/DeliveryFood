ed67c373ea311d70a396cecd1d4a77dab12e18e0
Vladimirova Ekaterina <echapyrkina@mail.ru>
2021-11-07 22:41:03 +0400

    new changes

--- js/partners.js
+++ js/partners.js
@@ -1,6 +1,43 @@
+const cardsRestaurants = document.querySelector('.cards-restaurants')
+
+console.dir(cardsRestaurants)
+
+
 const renderItems = (data) => {
-    data.forEach(elem => {
-        console.log(elem)
+    data.forEach((item) => {
+        const a = document.createElement('a')
+        const {image, kitchen, name, price, products, stars, time_of_delivery} = item
+
+        a.setAttribute('href', '/restaurant.html')
+        a.classList.add('card')
+        a.classList.add('cards-restaurants')
+        a.dataset.products = products
+
+            a.innerHTML = `<img src="${image}" alt="" class="card-image" />
+                            <div class="card-text">
+                                <div class="card-heading">
+                                    <h3 class="card-title">${name}</h3>
+                                    <span class="card-tag tag">${time_of_delivery} мин</span>
+                                </div>
+                                <div class="card-info">
+                                    <div class="rating">
+                                        ${stars}
+                                    </div>
+                                    <div class="price">От ${price} ₽</div>
+                                    <div class="category">${kitchen}</div>
+                                </div>
+                            </div>`
+
+        a.addEventListener('click', (e) => {
+            e.preventDefault()
+
+            localStorage.setItem('restaurant', JSON.stringify(item))
+
+            window.location.href = '/restaurant.html'
+
+        })
+
+        cardsRestaurants.append(a)
     });
 // console.log(data);
 }
